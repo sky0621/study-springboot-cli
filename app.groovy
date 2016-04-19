631efbed48107c8c0b5ec60f72e4369c711f41cd
S.S <ss@ss.com>
2016-04-20 00:46:49 +0900

    connect

--- app.groovy
+++ app.groovy
@@ -1,9 +1,11 @@
+@Grab("thymeleaf-spring4")
 @Controller
 class App {
 	@RequestMapping("/")
 	@ResponseBody
 	def home(ModelAndView mav) {
 		mav.setViewName("home")
+		mav.addObject("msg", "Hello! this is connect sample.")
 		mav
 	}
 }
